1ebce831a9cb476d5ae308356873b0245b8460a7
DR-84 <58080204+DR-84@users.noreply.github.com>
2020-02-11 15:24:29 +0100

    made arrows

--- calculatingWithFunctions/script.js
+++ calculatingWithFunctions/script.js
@@ -1,54 +1,30 @@
-function zero(a) {
-	return a ? a(0) : 0;
-}
-function one(a) {
-	return a ? a(1) : 1;
-}
-function two(a) {
-	return a ? a(2) : 2;
-}
-function three(a) {
-	return a ? a(3) : 3;
-}
-function four(a) {
-	return a ? a(4) : 4;
-}
-function five(a) {
-	return a ? a(5) : 5;
-}
-function six(a) {
-	return a ? a(6) : 6;
-}
-function seven(a) {
-	return a ? a(7) : 7;
-}
-function eight(a) {
-	return a ? a(8) : 8;
-}
-function nine(a) {
-	return a ? a(9) : 9;
-}
+const zero = a => (a ? a(0) : 0);
 
-function plus(b) {
-	return function(a) {
-		return Math.floor(a + b);
-	};
-}
-function minus(b) {
-	return function(a) {
-		return Math.floor(a - b);
-	};
-}
-function times(b) {
-	return function(a) {
-		return Math.floor(a * b);
-	};
-}
-function dividedBy(b) {
-	return function(a) {
-		return Math.floor(a / b);
-	};
-}
+const one = a => (a ? a(1) : 1);
+
+const two = a => (a ? a(2) : 2);
+
+const three = a => (a ? a(3) : 3);
+
+const four = a => (a ? a(4) : 4);
+
+const five = a => (a ? a(5) : 5);
+
+const six = a => (a ? a(6) : 6);
+
+const seven = a => (a ? a(7) : 7);
+
+const eight = a => (a ? a(8) : 8);
+
+const nine = a => (a ? a(9) : 9);
+
+const plus = b => a => Math.floor(a + b);
+
+const minus = b => a => Math.floor(a - b);
+
+const times = b => a => Math.floor(a * b);
+
+const dividedBy = b => a => Math.floor(a / b);
 
 console.log(seven(times(five())), 'test 35');
 console.log(four(plus(nine())), 'test 13');
